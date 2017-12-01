80c3a85fa574846b3238c41b95bd9442ec2fafaf
Daniel Watkins <daniel.watkins@canonical.com>
2017-12-01 17:32:14 -0500

    Produce separate speech output for responses

--- webhook.py
+++ webhook.py
@@ -1,6 +1,7 @@
 import requests
 from flask import Flask
 from flask_assistant import Assistant, ask, tell
+from num2words import num2words
 
 
 DOWNLOAD_STREAMS = (
@@ -33,18 +34,38 @@ def _find_latest_serial(ubuntu_release):
     return None
 
 
+def _speechify_serial(serial):
+    year, month, dayish = serial[2:4], serial[4:6], serial[6:]
+    if '.' in dayish:
+        day, after_dot = dayish.split('.')
+        parts = [
+            num2words(int(part)) for part in [year, month, day, after_dot]]
+        parts.insert(2, ',')
+        parts.insert(1, ',')
+        parts.insert(-1, 'dot')
+        speech_serial = ' '.join(parts)
+    else:
+        speech_serial = ', '.join(
+            num2words(int(part)) for part in [year, month, dayish])
+    return 'twenty ' + speech_serial
+
+
 @assist.action('find_latest_serial')
 def find_latest_serial(ubuntu_release):
     serial = _find_latest_serial(ubuntu_release)
     if serial is None:
-        speech = "Sorry, I couldn't find any release serials for {}".format(
-            ubuntu_release)
+        text = speech = (
+            "Sorry, I couldn't find any release serials for {}".format(
+                ubuntu_release))
     else:
-        speech = (
-            "The latest {} release serial on cloud-images.ubuntu.com is"
-            " {}".format(ubuntu_release, serial))
-    return tell(speech)
-
+        template = "The latest {release} release serial on {site} is {serial}"
+        text = template.format(release=ubuntu_release,
+                               site='cloud-images.ubuntu.com',
+                               serial=serial)
+        speech_serial = _speechify_serial(serial)
+        speech = template.format(release=ubuntu_release,
+                                 site='cloud images.ubuntu.com',
+                                 serial=speech_serial)
     return tell(speech, display_text=text)
 
 if __name__ == '__main__':
